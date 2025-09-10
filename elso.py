aee0bdeb51a1a5d84f757c1f506c5d3737f2453e
balazs4440 <mariapartics@gmail.com>
2025-09-10 12:27:36 +0200

    Óra vége

--- elso.py
+++ elso.py
@@ -12,8 +12,21 @@ for i in range(100):
 print(len(szamok),szamok)
 
 #egyszam jatek
+print("Számkitalálós játék!")
 jatek_szam=0
 nem_talaltDB=0
 
 kitalalando_szam = szamok[random.randint(len(szamok))]
 
+tipp = int(input("Tippelj egy pozitív egész számot!: "))
+while(tip!=kitalalando_szam):
+    print("Nem talált!")
+    tipp= int(input("Tippelj újra!: "))
+print("Kitaláltad!")
+
+folytat=input("Szeretnél még játszani? [I/N]")
+
+if(folytat=="I"):
+    #??????
+else:
+    exit()
