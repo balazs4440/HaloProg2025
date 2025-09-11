7d4a7cfbee8b15b5282a6426482599bdca6f5ce5
balazs4440 <mariapartics@gmail.com>
2025-09-11 08:09:23 +0200

    ver1

--- elso.py
+++ elso.py
@@ -2,31 +2,3 @@ import random
 
 #Lista
 szamok = []
-
-#Feltötése
-for i in range(100):
-    szam=random.randint(10, 99)
-    szamok.append(szam)
-
-#ell
-print(len(szamok),szamok)
-
-#egyszam jatek
-print("Számkitalálós játék!")
-jatek_szam=0
-nem_talaltDB=0
-
-kitalalando_szam = szamok[random.randint(len(szamok))]
-
-tipp = int(input("Tippelj egy pozitív egész számot!: "))
-while(tip!=kitalalando_szam):
-    print("Nem talált!")
-    tipp= int(input("Tippelj újra!: "))
-print("Kitaláltad!")
-
-folytat=input("Szeretnél még játszani? [I/N]")
-
-if(folytat=="I"):
-    #??????
-else:
-    exit()
