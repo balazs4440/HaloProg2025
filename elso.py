8fd984ee09607fc617a558c73c00e893733511a5
balazs4440 <mariapartics@gmail.com>
2025-09-11 09:19:29 +0200

    Alapjatek

--- elso.py
+++ elso.py
@@ -10,4 +10,26 @@ for i in range(100):
     szamok.append(szam)
 
 #ell
-print(len(szamok),szamok)
+#print(len(szamok),szamok)
+
+#egyszam jatek
+print("Számkitalálós játék!")
+jatek_szam=0
+nem_talaltDB=0
+
+kitalalando_szam = szamok[random.randint(0,len(szamok))]
+
+jatszol = True
+
+while(jatszol):
+    tipp = int(input("Tippelj egy pozitív egész számot!: "))
+
+    while(tipp!=kitalalando_szam):
+        print("Nem talált!")
+        tipp= int(input("Tippelj újra!: "))
+    print("Kitaláltad!")
+
+    folytat=input("Szeretnél még játszani? [I/N]")
+
+    if(folytat=="N"):
+        jatszol=False
