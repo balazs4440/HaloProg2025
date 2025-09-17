87781e7fad00f84f1676b41e9eaba57104909f4a
balazs4440 <mariapartics@gmail.com>
2025-09-17 10:24:14 +0200

    Kész

--- elso.py
+++ elso.py
@@ -14,7 +14,7 @@ while len(szamok)!=40:
 
 #egyszam jatek
 print("Számkitalálós játék!")
-jatek_szam=0
+jatek_szam=1
 nem_talaltDB=0
 
 kitalalando_szam = szamok[random.randint(0,len(szamok))]
@@ -27,17 +27,22 @@ while(jatszol):
         tipp= int(tipp_sz)
     else:
         print("Egész számmal játsz!")
+        tipp = 123
         continue
     
-    while(tipp!=kitalalando_szam):
-        if (tipp < kitalalando_szam):
+    while (tipp!=kitalalando_szam):
+        nem_talaltDB+=1
+        if (tipp == 123):
+            pass
+        elif (tipp < kitalalando_szam):
             print("Tippelj nagyobbat!")
-        else:
+        elif (tipp > kitalalando_szam):
             print("Tippelj kissebbet!")
         tipp_sz= input("[Kilépés X el lehetséges] \n Tippelj újra! :")
-        if(tipp_sz.isdecimal()):
+        if (tipp_sz.isdecimal()):
             tipp= int(tipp_sz)
         elif (tipp_sz == "X"):
+            print(f"{jatek_szam} db szám kitalálásához {nem_talaltDB}-szor próbálkozott rosszul.")
             exit()
         else:
             print("Egész számmal játsz!")
@@ -45,6 +50,8 @@ while(jatszol):
     print("Kitaláltad!")
 
     folytat=input("Szeretnél még játszani? [I/N]")
-
-    if(folytat=="N"):
+    if (folytat=="N"):
         jatszol=False
+        print(f"{jatek_szam} db szám kitalálásához {nem_talaltDB}-szor próbálkozott rosszul.")
+    elif (folytat=="I"):
+        jatek_szam+=1
